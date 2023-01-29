15629ad6572f4df43e0ac4cf044c2797067aeb13
gdj0208 <gdjsun@naver.com>
2023-01-29 21:18:41 +0900

    Initial Commit

---
+++ HelpingToolToDrink/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelpingToolToDrink
+//
+//  Created by 구동준 on 2023/01/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
